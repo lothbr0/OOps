dfbfbdf8945646ced2aa73c08c07e1faefa6f125
Shishanjan <Shishanjan.venkatesh@kalpitatechnologies.com>
2022-08-05 16:03:46 +0530

    First commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+
+    public class Calculate
+    {
+        public void MultiplyNumbers(double a, double b)
+        {
+            Console.WriteLine("a * b = {0}", a * b);
+        }
+        public void MultiplyNumbers(double a, double b, double c)
+        {
+            Console.WriteLine("a * b * c = {0}", a * b * c);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate c = new Calculate();
+            c.MultiplyNumbers(15.2, 25.1);
+            c.MultiplyNumbers(15.1, 25.6, 37);
+           
+            
+        }
+    }
+
