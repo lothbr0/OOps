7ce1b60ff9ed6c11192078fc3a0bce3fb82ac049
Shishanjan <Shishanjan.venkatesh@kalpitatechnologies.com>
2022-08-11 17:41:46 +0530

    first comit

---
+++ interface.cs
@@ -0,0 +1,25 @@
+﻿
+interface Sim
+{
+    void call(); void browse();
+}
+class Airtel : Sim
+{
+    public void call() { Console.WriteLine("Airtel is calling"); }
+    public void browse() { Console.WriteLine("Airtel 4G connecting"); }
+}
+class Jio : Sim
+{
+    public void call() { Console.WriteLine("Jio is calling"); }
+    public void browse() { Console.WriteLine("Jio 4G connecting"); }
+}
+class Program
+{
+    static void Main(String[] args)
+    {
+        Airtel a = new Airtel(); a.call(); a.browse(); Jio j = new Jio(); j.call(); j.browse();
+
+        Console.ReadLine();
+    }
+}
+
